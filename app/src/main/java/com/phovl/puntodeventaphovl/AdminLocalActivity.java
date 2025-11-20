3258c6dbc5aeedc92c62eda8ca4f9729d05b1311
ItsMazita <gael.gavu254@gmail.com>
2025-11-20 00:05:07 -0700

    Pequeños avances a la pantalla de administracion de local

--- app/src/main/java/com/phovl/puntodeventaphovl/AdminLocalActivity.java
+++ app/src/main/java/com/phovl/puntodeventaphovl/AdminLocalActivity.java
@@ -1,6 +1,8 @@
 package com.phovl.puntodeventaphovl;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,25 @@ public class AdminLocalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_local);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Obtenemos los datos enviados desde LocalesActivity
+        String nombreLocal = getIntent().getStringExtra("nombreLocal");
+        int imagenLocal = getIntent().getIntExtra("imagenLocal", 0);
+
+        //Buscamos los elementos en el layout
+        TextView txtNombre = findViewById(R.id.txtNombreLocalAdmin);
+        ImageView imgLocal = findViewById(R.id.imgLocalAdmin);
+
+        //Colocamos los datos
+        txtNombre.setText(nombreLocal);
+        if (imagenLocal != 0) {
+            imgLocal.setImageResource(imagenLocal);
+        }
     }
 }
